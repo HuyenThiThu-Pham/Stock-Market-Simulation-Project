8b50588e70fd0d199b1d3f5f43a300c566dda0b3
James Walsh <james.walsh@unisa.edu.au>
2024-06-07 09:24:55 +0930

    Update UntradedCompanyException.java

--- src/unisa/dse/a2/students/UntradedCompanyException.java
+++ src/unisa/dse/a2/students/UntradedCompanyException.java
@@ -1,8 +1,16 @@
 package unisa.dse.a2.students;
 
-public class UntradedCompanyException
+public class UntradedCompanyException extends Exception
 {
+	private static final long serialVersionUID = 1L;
+	
 	public UntradedCompanyException(String companyCode)
 	{
+		/**
+		 * The UntradedCompanyException class represents an exception that is thrown 
+		 * when an unknown company code is used in a trading operation. 
+		 */
+
+		super(companyCode + "is not a listed company on this exchange");
 	}
 }
